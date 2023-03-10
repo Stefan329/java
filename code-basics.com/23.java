b01a8260fa6b96f3b3d9645eecceccf47f264f71
Stefan329 <97978605+Stefan329@users.noreply.github.com>
2023-03-10 23:45:35 +0300

    Create 23.java

---
+++ code-basics.com/23.java
@@ -0,0 +1,9 @@
+class App {
+    public static void main(String[] args) {
+        var king = "King Balon the 6th";
+        var theFirstNumber = 6;
+        var theSecondNumber = 17;
+        var coin = theFirstNumber * theSecondNumber;
+        System.out.println(king + " has " + coin + " rooms.");
+    }
+}
