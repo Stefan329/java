65015fd8565dfe57bac61ef9b619f04c66c88902
Stefan329 <97978605+Stefan329@users.noreply.github.com>
2023-03-11 00:26:41 +0300

    Create 27.java

---
+++ code-basics.com/27.java
@@ -0,0 +1,8 @@
+class App {
+    public static void main(String[] args) {
+        String cat = "hexlet";
+        char coin = '-';
+        int num = 7;
+        System.out.println(cat + coin + num);
+    }
+}
