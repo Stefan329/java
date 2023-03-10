3481965f39eabba4700c905990efc25c71b86ef8
Stefan329 <97978605+Stefan329@users.noreply.github.com>
2023-03-10 22:17:23 +0300

    Create 15.java

---
+++ code-basics.com/15.java
@@ -0,0 +1,5 @@
+class App {
+    public static void main(String[] args) {
+        System.out.println("Winter" + " " + "came" + " " + "for" + " " + "the" + " " + "House " + "of " + "Frey.");
+    }
+}
