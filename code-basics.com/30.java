7420bf3ecd35cfa41d5a2b716a5bc82a0dfc8723
Stefan329 <97978605+Stefan329@users.noreply.github.com>
2023-03-11 00:41:42 +0300

    Create 30.java

---
+++ code-basics.com/30.java
@@ -0,0 +1,9 @@
+class App {
+    public static void main(String[] args) {
+        var company1 = "Apple";
+        var company2 = "Samsung";
+        var coin1 = company1.length();
+        var coin2 = company2.length();
+        System.out.println(coin1 + coin2);
+    }
+}
