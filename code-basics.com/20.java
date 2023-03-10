0e305d01d0040788c9adeae9dbf43e1f3e35a307
Stefan329 <97978605+Stefan329@users.noreply.github.com>
2023-03-10 23:12:03 +0300

    Create 20.java

---
+++ code-basics.com/20.java
@@ -0,0 +1,8 @@
+class App {
+    public static void main(String[] args) {
+        var eurosCount = 100;
+        var dollarsCount = eurosCount * 1.25;
+        var rublesCount = dollarsCount * 60;
+        System.out.println(dollarsCount); System.out.println(rublesCount );
+    }
+}
