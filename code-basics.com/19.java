c06dcfbd817ad2df4604a81b1239525b0c14de38
Stefan329 <97978605+Stefan329@users.noreply.github.com>
2023-03-10 22:44:07 +0300

    Create 19.java

---
+++ code-basics.com/19.java
@@ -0,0 +1,7 @@
+class App {
+    public static void main(String[] args) {
+        var family = "Targaryen";
+        var pet = "Dragon";
+        System.out.println(family + " and " + pet);
+    }
+}
