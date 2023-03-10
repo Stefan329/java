c8052397dfd6b4ec4ad585b75b6c9d8a6f3534cb
Stefan329 <97978605+Stefan329@users.noreply.github.com>
2023-03-11 01:21:40 +0300

    Create 33.java

---
+++ code-basics.com/33.java
@@ -0,0 +1,8 @@
+class App {
+    public static void main(String[] args) {
+        var email = " SupporT@hexlet.io\n";
+        var cat = email.trim();
+        email = cat.toLowerCase();
+        System.out.println(email);
+    }
+}
