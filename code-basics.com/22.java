3076d3241f77f2077c26a5c7a7152c13385051cc
Stefan329 <97978605+Stefan329@users.noreply.github.com>
2023-03-10 23:32:36 +0300

    Create 22.java

---
+++ code-basics.com/22.java
@@ -0,0 +1,7 @@
+class App {
+    public static void main(String[] args) {
+        var theFirstNumber = 11;
+        var theSecondNumber = -100;
+        System.out.println(theFirstNumber * theSecondNumber);
+    }
+}
