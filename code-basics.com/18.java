618411bd02fdabea1ead725e4006bd491cf0f043
Stefan329 <97978605+Stefan329@users.noreply.github.com>
2023-03-10 22:35:58 +0300

    Create 18.java

---
+++ code-basics.com/18.java
@@ -0,0 +1,7 @@
+class App {
+    public static void main(String[] args) {
+        var The_first_number = 1.10;
+        var The_second_number = -100;
+        System.out.println(The_first_number * The_second_number);
+    }
+}
