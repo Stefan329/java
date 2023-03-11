d841acbb4524782eec1cd565cfad34063e75e0d7
Stefan329 <97978605+Stefan329@users.noreply.github.com>
2023-03-11 08:03:06 +0300

    Create 52.java

---
+++ code-basics.com/52.java
@@ -0,0 +1,14 @@
+public class App {
+    public static String getNumberExplanation(int number) {
+        switch (number) {
+            case 666:
+                return "devil number";
+            case 7:
+                return "prime number";
+            case 42:
+                return "answer for everything";
+            default:
+                return null;
+        }
+    }
+}
