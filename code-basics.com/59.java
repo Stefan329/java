3edafb4fc121df67a53e7d5c8ac139b07153e9a2
Stefan329 <97978605+Stefan329@users.noreply.github.com>
2023-03-11 08:13:10 +0300

    Create 59.java

---
+++ code-basics.com/59.java
@@ -0,0 +1,14 @@
+public class App {
+    public static String filterString(String str, char ch) {
+        var i = 0;
+        var result = "";
+        while (i < str.length()) {
+            var currentChar = str.charAt(i);
+            if (currentChar != ch) {
+                result += currentChar;
+            }
+            i += 1;
+        }
+        return result;
+    }
+}
