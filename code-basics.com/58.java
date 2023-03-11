02d5e19be9401a0ab8724ac5246845887c3881e4
Stefan329 <97978605+Stefan329@users.noreply.github.com>
2023-03-11 08:11:49 +0300

    Create 58.java

---
+++ code-basics.com/58.java
@@ -0,0 +1,11 @@
+public class App {
+    public static String reverse(String str) {
+        var i = str.length() - 1;
+        var result = "";
+        while (i >= 0) {
+            result = result + str.charAt(i);
+            i = i - 1;
+        }
+        return result;
+    }
+}
