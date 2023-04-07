c1d651e3bfc4aeae70f2005fe4403f2d4d52d653
Stefan329 <97978605+Stefan329@users.noreply.github.com>
2023-04-07 19:23:42 +0300

    Create 1.java

---
+++ КТ-2/1.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Main {
+    public static void main(String []args) {
+        Scanner sc=new Scanner(System.in);
+        int m,n;
+        System.out.println("Введите кол-во строк: ");
+        m=sc.nextInt();
+
+        System.out.println("Введите кол-во столбцов: ");
+        n=sc.nextInt();
+        int cat[][]=new int[10][10];
+        System.out.println("Введите элементы матрицы: ");
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cat[i][j]=sc.nextInt();
+            }
+        }
+        
+        int dog[][]=new int[10][10];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                dog[j][i]=cat[i][j];
+            }
+        }
+
+        System.out.println("Элементы матрицы после транспонирования");
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print(dog[i][j]+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
